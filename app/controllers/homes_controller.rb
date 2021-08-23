d38018871982244ac5adfa0da201badc6d46b0ea
gensan-64 <genyuu6240@gmail.com>
2021-08-23 05:02:45 +0000

    finish

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,15 @@
-class Controller < ApplicationController
+class HomesController < ApplicationController
   def top
   end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
 end
---
+++ db/migrate/20210823013900_add_taitle_to_books.rb
@@ -0,0 +1,5 @@
+class AddTaitleToBooks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :books, :title, :text
+  end
+end
